cf00c3080ed0959886347088f920b73b60f86f60
Aliaksandr Yalouskikh <alexyo87@yahoo.com>
2022-05-17 11:34:08 +0700

    made token contract cleaner

--- contracts/Token1155.sol
+++ contracts/Token1155.sol
@@ -7,23 +7,18 @@ import "@openzeppelin/contracts/utils/Strings.sol";
 
 contract Token1155 is ERC1155, Ownable {
     
-    string public name; //NFT name
-    uint256 public quantity; //quantity to mint on contract deployment
-    address public approvedAddress; //airdrop contract address
+    string public name; //NFT collection name
     string public path = "https://gateway.pinata.cloud/ipfs/XXX/"; //initial uri. Replace triple X with your collection IPFS
 
-    constructor(string memory _name, uint256 _quantity, uint256 _id, address _address)
+    constructor(string memory _name, uint256 _quantity, uint256 _id, address _airdropContractAddress)
         ERC1155("https://gateway.pinata.cloud/ipfs/XXX/{id}.json") //replace triple X with your collection IPFS
     {
         name = _name;
-        quantity = _quantity;
-        id = _id;
-        approvedAddress = _address;
-        _mint(msg.sender, id, quantity, "");
-        _setApprovalForAll(msg.sender, approvedAddress, true);
+        _mint(msg.sender, _id, _quantity, "");
+        _setApprovalForAll(msg.sender, _airdropContractAddress, true);
     }
 
-    function uri(uint256 _tokenId) override public pure returns (string memory) {
+    function uri(uint256 _tokenId) override public view returns (string memory) {
         return string(
             abi.encodePacked(
                 path, 
